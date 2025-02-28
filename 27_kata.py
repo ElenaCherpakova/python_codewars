a8bd83ff2571a4a53afc1764cc822a4332a0d04a
ElenaCherpakova <elena.cherpakova@gmail.com>
2025-02-27 23:53:01 -0500

    solved 27th

---
+++ 27_kata.py
@@ -0,0 +1,24 @@
+# Write a function that takes as parameters an array (arr) and 2 integers (x and y). The function should return the mean between the mean of the the first x elements of the array and the mean of the last y elements of the array.
+
+# The mean should be computed if both x and y have values higher than 1 but less or equal to the array's length. Otherwise the function should return -1.
+
+# Examples
+# [1, 3, 2, 4], 2, 3 => should return 2.5
+# because: the mean of the the first 2 elements of the array is (1+3)/2=2 and the mean of the last 3 elements of the array is (4+2+3)/3=3 so the mean of those 2 means is (2+3)/2=2.5.
+
+# [1, 3, 2, 4], 1, 2 => should return -1
+# because x is not higher than 1.
+
+# [1, 3, 2, 4], 2, 8 => should return -1
+
+def get_mean(arr,x,y):
+    if 1 < x <= len(arr) and 1 < y <= len(arr):
+        sum_firs = sum(arr[:x]) / x
+        sum_last = sum(arr[-y:]) / y
+        return (sum_firs + sum_last) / 2
+    else:
+        return -1
+
+print(get_mean([1,2,3,4],2,3)) #2.5
+print(get_mean([1,2,3,4],1,2)) #-1
+print(get_mean([1,2,3,4],2,8)) #-1
